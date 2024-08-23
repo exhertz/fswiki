25665bdc688f2fae55dff856d297fa9cbf856a0d
Alexandr <karmak.alexander032@yandex.ru>
2024-08-23 14:56:06 +0300

    fix: delete dublicate meta theme-color & new styles for comments

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -22,7 +22,6 @@ export const shared = defineConfig({
     ['link', { rel: 'manifest', href: '/site.webmanifest'}],
     ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'}],
     ['meta', { name: 'msapplication-TileColor', content: '#da532c'}],
-    ['meta', { name: 'theme-color', content: '#ffffff'}],
   ],
 
   themeConfig: {
