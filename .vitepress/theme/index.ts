fcbe178ef9be562918240bc897187dcdb0df2240
Alexandr <karmak.alexander032@yandex.ru>
2024-08-25 19:19:29 +0300

    vp: add plugin image viewer to zoom image. - add pointer cursor to img

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,18 @@
 // https://vitepress.dev/guide/custom-theme
 import { h, defineComponent, inject } from 'vue'
 import { useRoute, type Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
-import './fonts.css'
-import './style.css'
 import { setup } from '@css-render/vue3-ssr'
 import { NConfigProvider } from 'naive-ui'
 
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
+
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+
+import 'viewerjs/dist/viewer.min.css';
+import './fonts.css'
+import './style.css'
 
 const CssRenderStyle = defineComponent({
   setup () {
@@ -51,11 +56,16 @@ export default {
   extends: DefaultTheme,
   Layout: NaiveUIProvider,
   enhanceApp: ({ app }) => {
+    app.use('vImageViewer', vImageViewer);
     if (import.meta.env.SSR) {
       const { collect } = setup(app)
       app.provide('css-render-collect', collect)
     }
-  }
+  },
+  setup() {
+    const route = useRoute();
+    imageViewer(route);
+}
 } satisfies Theme
 
 // export default {
