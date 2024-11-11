5d673cd7f1b7bac0a311d3ffe49f2388c3fe4362
exhertz <cactuseveryday@tuta.io>
2024-11-11 20:23:04 +0300

    vp/vue: change all imageviewer (viewer.js) to naive and cutomization

---
+++ .vitepress/theme/components/ImageView.vue
@@ -0,0 +1,23 @@
+<template>
+    <n-image
+        v-bind="$attrs"
+        :render-toolbar="renderToolbar"
+    />
+</template>
+
+<script setup lang="ts">
+import { NImage } from 'naive-ui'
+import type { ImageRenderToolbarProps } from 'naive-ui'
+
+const renderToolbar = ({ nodes }: ImageRenderToolbarProps) => [
+    nodes.zoomIn,
+    nodes.zoomOut,
+    nodes.prev,
+    nodes.next,
+    nodes.rotateClockwise,
+    nodes.close
+]
+</script>
+
+<style scoped>
+</style>
