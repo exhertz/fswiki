bdb0311fc4f2b5b63616b7faa8a4f761359021bf
exhertz <cactuseveryday@tuta.io>
2024-11-11 20:24:52 +0300

    chore: remove viewerjs & vitepress-plugin-image-viewer

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,10 +7,6 @@ import { NConfigProvider } from 'naive-ui'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-
-import 'viewerjs/dist/viewer.min.css';
 import './fonts.css'
 import './style.css'
 
@@ -56,16 +52,12 @@ export default {
   extends: DefaultTheme,
   Layout: NaiveUIProvider,
   enhanceApp: ({ app }) => {
-    app.use('vImageViewer', vImageViewer);
     if (import.meta.env.SSR) {
       const { collect } = setup(app)
       app.provide('css-render-collect', collect)
     }
   },
-  setup() {
-    const route = useRoute();
-    imageViewer(route);
-}
+  setup() {}
 } satisfies Theme
 
 // export default {
